1069cebd5acde17179512ddc4195e78e649e01e4
phoenix913 <ivarkallejarv@khk.ee>
2015-09-03 15:48:52 +0300

    Lisasin taustavärvi. Lisasin pealkirja.

---
+++ SoulsRepo.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+        body{background-color(#76C4AE);}
+
+    </style>
+    <meta charset="UTF-8">
+    <title>Veebileht</title>
+</head>
+<body>
+<h1>Veebileht</h1>
+</body>
+</html>
