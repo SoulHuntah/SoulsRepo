6415946524e503fb84397ccfa263bf426e5275a7
phoenix913 <ivarkallejarv@khk.ee>
2015-09-03 16:13:52 +0300

    Lisasin pildi.

--- SoulsRepo.php
+++ SoulsRepo.php
@@ -3,12 +3,12 @@
 <head>
     <style>
         body{background-color(#76C4AE);}
-
     </style>
     <meta charset="UTF-8">
     <title>Veebileht</title>
 </head>
 <body>
 <h1>Veebileht</h1>
+<img src="Facebooki pilt.jpg" alt="Pilt facebookist" style="height:auto;width:auto">
 </body>
 </html>
